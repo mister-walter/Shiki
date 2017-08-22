51acbf6afd185ef4714a1a4287c3d38a8f3c8dfd
Rachel Plante <rplante@wpi.edu>
2017-08-22 13:53:48 +0900

    SeasonScript
    Script that changes color of objects based on what season they are in. Add this script as a component to an object that should be seasonally affected.

---
+++ Assets/Scripts/SeasonScripts/SeasonalEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonalEffect : MonoBehaviour {
+
+    private string seasonName;
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateColor();
+	}
+
+    void UpdateColor ()
+    {
+        seasonName = this.gameObject.scene.name;
+
+        switch (seasonName)
+        {
+            case "Winter": this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue); break; //turns blue
+            case "Spring": this.GetComponent<Renderer>().material.SetColor("_Color", Color.magenta); break; //turns pink
+            case "Summer": this.GetComponent<Renderer>().material.SetColor("_Color", Color.green); break; //turns green
+            case "Fall": this.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow); break; //turns yellow
+            default: return; //unexpected? Don't change
+        }
+    }
+}
