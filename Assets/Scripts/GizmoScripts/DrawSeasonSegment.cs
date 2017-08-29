e707f7351c26b6b608ff6cf58a6416808e7b7a13
Andrew Walter <atwalter@wpi.edu>
2017-08-29 15:24:32 +0900

    Should have fixed serialization error caused by gizmo drawing scripts

--- Assets/Scripts/GizmoScripts/DrawSeasonSegment.cs
+++ Assets/Scripts/GizmoScripts/DrawSeasonSegment.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if (UNITY_EDITOR)
 using UnityEngine;
-#if (UNITY_EDITOR)
+#endif
+
 public class DrawSeasonSegment : MonoBehaviour {
     public Color color = Color.green;
     public float startAngle = 0;
     private float radius = 60;
     void OnDrawGizmos()
     {
+        #if (UNITY_EDITOR)
         var firstPoint = new Vector3(Mathf.Cos(Mathf.Deg2Rad * startAngle), 0, Mathf.Sin(Mathf.Deg2Rad * startAngle)) * radius;
         var secondPoint = new Vector3(Mathf.Cos(Mathf.Deg2Rad * (startAngle + 90)), 0, Mathf.Sin(Mathf.Deg2Rad * (startAngle + 90))) * radius;
         UnityEditor.Handles.color = color;
@@ -15,6 +16,6 @@ public class DrawSeasonSegment : MonoBehaviour {
         UnityEditor.Handles.DrawLine(firstPoint, firstPoint * 0.5f);
         UnityEditor.Handles.DrawLine(secondPoint, secondPoint * 0.5f);
         UnityEditor.Handles.DrawLine(firstPoint * 0.5f, secondPoint * 0.5f);
+        #endif
     }
 }
-#endif
