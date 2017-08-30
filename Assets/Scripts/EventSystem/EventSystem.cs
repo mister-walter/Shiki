17e543896c6bd8716900d0252d1431442fe5e0e2
Andrew Walter <atwalter@wpi.edu>
2017-08-30 11:44:46 +0900

    Initial version of EventSystem completed

--- Assets/Scripts/EventSystem/EventSystem.cs
+++ Assets/Scripts/EventSystem/EventSystem.cs
@@ -3,20 +3,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IEvent {}
+public interface IGameEvent {}
 
-public delegate void GameEventHandler(GameObject sender, object arg);
+public delegate void GameEventDelegate<T>(T evt) where T: IGameEvent;
 
 public class GameEventSystem {
-    public static Dictionary<Type, List<GameEventHandler>> eventMap;
-    public static void AddListener(IEvent evt, GameEventHandler handler)
+    private static Dictionary<Type, Delegate> eventMap;
+
+    /// <summary>
+    /// Add a listener for an event. 
+    /// The event to listen for is determined through the generic type parameter of the given Action
+    /// </summary>
+    /// <typeparam name="T">The IGameEvent to attach the given delegate to</typeparam>
+    /// <param name="del">The Action to perform when the event occurs</param>
+    public static void AttachDelegate<T>(Action<T> del) where T: IGameEvent
+    {
+        var evtType = typeof(T);
+        if(!eventMap.ContainsKey(evtType)) {
+            eventMap[evtType] = del;
+        } else {
+            eventMap[evtType] = Delegate.Combine(eventMap[evtType], del);
+        }
+    }
+
+    /// <summary>
+    /// Remove a listener from an event. 
+    /// The event to remove from is determined through the generic type parameter of the given Action
+    /// </summary>
+    /// <typeparam name="T">The IGameEvent to remove the given delegate from</typeparam>
+    /// <param name="del">The Action to remove from the event</param>
+    public static void RemoveDelegate<T>(Action<T> del) where T : IGameEvent
+    {
+        var evtType = typeof(T);
+        if (eventMap.ContainsKey(evtType))
+        {
+            var newDel = Delegate.Remove(eventMap[evtType], del);
+            if(newDel == null)
+            {
+                eventMap.Remove(evtType);
+            } else
+            {
+                eventMap[evtType] = newDel;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Causes an event to fire, causing any listening delegates to be run.
+    /// </summary>
+    /// <param name="evt">The event to fire. This is passed on to the delegates, so this is how to pass information to the delegates.</param>
+    public static void FireEvent(IGameEvent evt)
     {
         var evtType = evt.GetType();
-        if(!eventMap.ContainsKey(evtType))
+        if(eventMap.ContainsKey(evtType))
         {
-            eventMap[evtType] = new List<GameEventHandler>();
+            eventMap[evtType].DynamicInvoke(evt);
         }
-        var listenerList = eventMap[evtType];
-        listenerList.Add(handler);
     }
 }
