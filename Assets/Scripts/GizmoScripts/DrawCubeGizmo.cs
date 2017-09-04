071c0ab2925aba104e53b19c3da9bcfdaf43934c
Andrew Walter <atwalter@wpi.edu>
2017-09-04 17:11:53 +0900

    Rotated seasons and updated scripts
    Rotated the season ground models into their proper positions
    
    Updated the SeasonCoordinateManager AngleInQuadrant function to work with seasons aligned to 0 degrees instead of 45 degrees.
    
    Replaced old season diamond model with a new one. The old one was slightly incorrect.
    
    Updated DrawCubeGizmo to take into account the rotation of the underlying cube.
    
    Updated DrawSeasonSegment to simply draw a wireframe of the mesh object it is attached to.

--- Assets/Scripts/GizmoScripts/DrawCubeGizmo.cs
+++ Assets/Scripts/GizmoScripts/DrawCubeGizmo.cs
@@ -6,6 +6,8 @@ public class DrawCubeGizmo : MonoBehaviour {
     {
         #if (UNITY_EDITOR)
         UnityEditor.Handles.color = color;
+        
+        UnityEditor.Handles.matrix *= Matrix4x4.TRS(Vector3.zero, this.gameObject.transform.rotation, Vector3.one);
         UnityEditor.Handles.DrawWireCube(transform.position, transform.localScale);
         #endif
     }
