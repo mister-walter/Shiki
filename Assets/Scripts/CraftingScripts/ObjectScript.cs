6f1416d6dfc46cfff27bd77a53595910343b466b
Rachel Plante <rplante@wpi.edu>
2017-09-12 15:21:51 +0900

    Added tool and object scripts for testing
    Testing to see whether current object/tool scripts work in VR environment. Currently tool collisions in PC environment do not have velocity magnitude when tool is held (isKinematic = true)

---
+++ Assets/Scripts/CraftingScripts/ObjectScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScript : MonoBehaviour
+{
+
+    //private string tooltype;
+    private int hp;
+    private Material newMaterial;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+       //tooltype = "stick";
+        hp = 15;
+    }
+
+    public void hit(float velocityMag)
+    {
+        hp -= (int)Mathf.Floor(velocityMag);
+
+        if (hp <= 0)
+        {
+            hp = 0;
+            changeObject();
+        }
+    }
+
+    public void changeObject()
+    {
+        GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Cube); //create a new object
+
+        Vector3 pos = transform.position;   //get old object position
+        Vector3 scale = transform.localScale; //get old object size
+        Quaternion rotation = transform.rotation; //get old object rotation
+        newMaterial = Resources.Load("AffectedCube", typeof(Material)) as Material; //load up the new material
+        newObject.GetComponent<Renderer>().material = newMaterial; //assign new material to object
+
+        newObject.transform.position = pos; //assign old object's position to new object
+        newObject.transform.localScale = scale; //assign old object's scale to new object
+        newObject.transform.rotation = rotation; //assign old object's rotation to new object
+
+        DestroyObject(gameObject); //destroy the old object
+    }
+
+}
---
+++ Assets/Scripts/CraftingScripts/ToolScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolScript : MonoBehaviour {
+
+    private GameObject collidingObject;
+
+    //public ObjectScript colObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        collidingObject = col.gameObject;
+        float strength = col.relativeVelocity.magnitude;
+
+        if (collidingObject.GetComponent("ObjectScript"))
+        {
+            collidingObject.GetComponent<ObjectScript>().hit(strength);
+            Debug.Log(strength);
+        }
+        /*collidingObject = col.gameObject;
+        ObjectScript other = (ObjectScript) collidingObject.GetComponent(typeof(ObjectScript));
+        other.changeObject();*/
+    }
+}
