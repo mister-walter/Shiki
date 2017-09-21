db59596f0f6d0de211e29b3b08108d18f35761f6
Rachel Plante <rplante@wpi.edu>
2017-09-21 19:49:49 +0900

    Added new tool recognition and wield tool scripts
    Haven't tested new tool script yet, but wanted to just get it out there. The wield tool script isn't done yet because it's extra and I'm tired rn. Should be like a 5 minute implementation though

---
+++ Assets/Scripts/CraftingScripts/AdvancedToolScript.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AdvancedToolScript : MonoBehaviour {
+
+    Vector3 oldPos, newPos, deltaPos; //for storing the current position, the next position, and the change in those two
+    DateTime oldTime, newTime; //for storing the current time and the next time
+    Int32 deltaTime; //for storing the difference in time recorded (in ms)
+    enum behavior { NONE, POUNDING, CHOPPING, DIGGING, PAINTING }; //labels for which activity is occuring
+    behavior activity, lastKnown; //for storing the current activity and the last activity known
+    float force; //for storing the collision velocity for determining object damage
+    int numHits; //for keeping track of how many collisions have occurred
+    public int hitThreshold; //how many collisions needed to occur to determine a behavior
+    public int attentionSpan; //for determining if the length of time between collisions is concurrent with performing a behavior
+    public float posXminThreshold, posYminThreshold, posZminThreshold; //thresholds determining how miniscule change must be to ignore
+    public float posXmaxThreshold, posYmaxThreshold, posZmaxThreshold; //thresholds determining max limit of change to determine behavior
+
+
+	// Use this for initialization
+	void Start () {
+        numHits = 0;
+        hitThreshold = 2;           //can be set in editor
+        posXminThreshold = 0.5f;    //can be set in editor
+        posYminThreshold = 0.5f;    //can be set in editor
+        posZminThreshold = 0.5f;    //can be set in editor
+        posXmaxThreshold = 1.5f;    //can be set in editor
+        posYmaxThreshold = 1.5f;    //can be set in editor 
+        posZmaxThreshold = 1.5f;    //can be set in editor
+        attentionSpan = 1500;       //can be set in editor
+        activity = behavior.NONE;
+        lastKnown = behavior.NONE;
+        force = 0;
+	}
+	
+    public void OnCollisionEnter(Collision col)
+    {
+        //initialize position and time values
+        if(numHits == 0) //no prior collision
+        {
+            oldPos = col.contacts[0].point; //get the first point to collide, I don't even give a shit which one it is tbh
+            newPos = col.contacts[0].point; //get the first point to collide
+            oldTime = DateTime.UtcNow;
+            newTime = DateTime.UtcNow;
+        }
+        else //prior collision detected
+        {
+            oldPos = newPos;
+            newPos = col.contacts[0].point; //get the first point to collide, I don't even give a shit which one it is tbh
+            oldTime = newTime;
+            newTime = DateTime.UtcNow;
+        }
+        //calculate deltas
+        deltaPos = newPos - oldPos;
+        deltaTime = (Int32) newTime.Subtract(oldTime).TotalMilliseconds;
+        //increment number of collisions detected
+        numHits += 1;
+        //keep track of how much damage to deal if behavior is classified
+        force += col.relativeVelocity.magnitude;
+
+        //determine if a behavior might be happening
+        if(numHits >= hitThreshold && deltaTime <= attentionSpan)
+        {
+            classify(col);
+        }
+
+    }
+
+    void classify(Collision col)
+    {
+        //focus on x, ignore y and z
+        if (Math.Abs(deltaPos.x) <= posXmaxThreshold && (Math.Abs(deltaPos.y) > posYminThreshold && Math.Abs(deltaPos.z) > posZminThreshold))
+        {
+            activity = behavior.PAINTING;
+            if (activity != lastKnown && lastKnown != behavior.NONE) //user isn't hitting object in line with any behavior
+                resetTracking();
+            else
+            {
+                //col.gameObject.hit(activity, force);
+                force = 0; //reset so that force does not linearly increase if activity continues (don't deal damage that's already been dealt)
+                lastKnown = activity;
+            }
+        }
+        //focus on y, ignore x and z
+        else if (Math.Abs(deltaPos.y) <= posYmaxThreshold && (Math.Abs(deltaPos.x) > posXminThreshold && Math.Abs(deltaPos.z) > posZminThreshold))
+        {
+            activity = behavior.CHOPPING;
+            if (activity != lastKnown && lastKnown != behavior.NONE) //user isn't hitting object in line with any behavior
+                resetTracking();
+            else
+            {
+                //col.gameObject.hit(activity, force);
+                force = 0; //reset so that force does not linearly increase if activity continues (don't deal damage that's already been dealt)
+                lastKnown = activity;
+            }
+        }
+        //focus on z, ignore x and y
+        else if (Math.Abs(deltaPos.z) <= posZmaxThreshold && (Math.Abs(deltaPos.x) > posXminThreshold && Math.Abs(deltaPos.y) > posYminThreshold))
+        {
+            activity = behavior.POUNDING;
+            if (activity != lastKnown && lastKnown != behavior.NONE) //user isn't hitting object in line with any behavior
+                resetTracking();
+            else
+            {
+                //col.gameObject.hit(activity, force);
+                force = 0; //reset so that force does not linearly increase if activity continues (don't deal damage that's already been dealt)
+                lastKnown = activity;
+            }
+        }
+        else
+            return;
+    }   
+
+    void resetTracking()
+    {
+        numHits = 0;
+        activity = behavior.NONE;
+        lastKnown = behavior.NONE;
+        force = 0;
+    }
+}
---
+++ Assets/Scripts/VRUtilities/WieldTool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WieldTool : MonoBehaviour {
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId padButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool triggerButtonDown = false;
+
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    // device to get easy access to the controller
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    // reference to object being tracked
+    private SteamVR_TrackedObject trackedObj;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*
+     *
+     * fuck it, I'll finish this later 
+     * 
+     * (but basically, if the player double clicks the trigger button near the tool object, they 
+     *  will then wield the tool object. This will prevent hand fatigue since they won't have to 
+     *  physically be holding the tool the entire time they want to use it. This will also orient
+     *  the tool in such a way that it's always in the proper position in their hand, so that it 
+     *  doesn't matter what angle their hand approaches it from when they go to pick it up. When 
+     *  they are done holding the tool and want to drop it, they will double click the trigger 
+     *  button again and the tool will drop. While they're holding the tool, they should not be 
+     *  allowed to pick up another object with that hand. This script, once working, should probably 
+     *  just be added as an extension of GrabObject.cs so that we don't have two competing functionalities.)
+     * 
+     */
+}
